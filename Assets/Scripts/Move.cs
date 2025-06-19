d4327636f07eb0cbe0cc3c94c5ef7f1d05b6fa43
Code Ninjas Etobicoke <jcarr701@gmail.com>
2025-06-19 17:55:09 -0400

    h

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public Rigidbody rb;
-    float planeRotation = 0;
 
     public float UpForce = 5000f;
     public float DownForce = 5000f;
+    public float rotationalForce = 1f;
+
+    private float rotationBuffer = 0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,30 +22,46 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        planeRotation = planeRotation * 0.9f;
 
         //transform.rotation = Quaternion.Euler(0, transform.rotation.y, planeRotation);
-
+        Debug.Log(transform.rotation.y);
+        //Tilt Left
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(-Vector3.up * 100 * Time.deltaTime);
-            //planeRotation = -50f;
+            rotationBuffer += rotationalForce;
+            transform.Rotate(new Vector3(0f, -rotationBuffer, 0f) * Time.deltaTime);
         }
+        //Tilt Right
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.up * 100 * Time.deltaTime);
-            planeRotation = 50f;
+            rotationBuffer += rotationalForce;
+            transform.Rotate(new Vector3(0f, rotationBuffer, 0f) * Time.deltaTime);
         }
+
+
+        //Tilt Up
         if (Input.GetKey(KeyCode.S))
         {
             transform.Rotate(Vector3.left * 10f * Time.deltaTime);
             rb.AddForce(transform.up * UpForce * Time.deltaTime);
         }
+        //Tilt Down
         if (Input.GetKey(KeyCode.W))
         {
             transform.Rotate(-Vector3.left * 10f * Time.deltaTime);
             rb.AddForce(-transform.up * DownForce * Time.deltaTime);
         }
+        //Roll Counter clockwise
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, 0, 10) * Time.deltaTime);
+        }
+        //Roll Clockwise
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 0, -10) * Time.deltaTime);
+        }
+        //Boosts Forward
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddForce(transform.forward * 50f);
